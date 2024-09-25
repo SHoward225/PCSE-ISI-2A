e7d8501bbeca383f12628caf9f3e39a397e5418a
Konan Junior Kouadio <stephane.kouadio@grenoble-inp.org>
2024-09-25 20:16:30 +0200

    implementation des fonctions place curseur et defilement

--- ecran.c
+++ ecran.c
@@ -15,8 +15,9 @@
 #define MEMOIRE_VIDEO_BASE 0xB8000 //adresse de base de la memoire video CGA
 
 
-// static uint32_t position_colonne =0 ;
-// static uint32_t position_ligne =0 ;
+static uint32_t position_colonne =0 ;
+static uint32_t position_ligne =0 ;
+
 
 // ************** DATA STRUCTURES DEFINITION **************
 enum {
@@ -79,8 +80,15 @@ void efface_ecran(void){
 /*
  * Place le curseur a la position donnée 
  */
+
 void place_curseur(uint32_t lig, uint32_t col){
-    /*CODE*/
+    uint16_t position = col + lig * COL_MAX;
+    // Envoie de la commande 0x0F sur la partie basse
+    outb(0x0F, 0x3D4);
+    outb(position, 0x3D5);
+    // Envoie de la commande 0x0F sur la partie basse
+    outb(0x0E, 0x3D4);
+    outb(position >> 8, 0x3D5);
 }
 
 /*
@@ -88,7 +96,63 @@ void place_curseur(uint32_t lig, uint32_t col){
  * implemante l'effet voulu si c'est un caractere de controle) 
  */
 void traite_car(char c){
-    /*CODE*/
+    switch (c)
+    {
+        //Recule curseur
+        case '\b':  // Backspace
+            if (position_colonne != 0) position_colonne--;
+            break;
+
+        //Avance a la prochaine tabulation
+        case '\t':  // Tabulation
+            position_colonne = (position_colonne + 8) & ~(8 - 1);
+            break;
+        
+        //Retour à la ligne et col=0
+        case '\n':  // Nouvelle ligne
+            position_ligne = 0;
+            position_ligne++;
+            break;
+
+        //Efface ecran et place curseur en (0;0)
+        case '\f':
+            efface_ecran();
+            position_colonne = 0;
+            position_ligne = 0;
+            break;
+
+        //Deplace curseur sur la ligne actuelle et col=0
+        case '\r':
+            position_colonne = 0;
+            break;
+
+        //Par defaut
+        default:
+            if (c >= 32 && c <= 126) {
+                ecrit_car(position_ligne, position_colonne, c, 15, 0, 5);
+                position_colonne++;
+            }
+            break;
+    }
+
+    // Vérification des limites
+    if (position_colonne >= COL_MAX) {
+        position_colonne = 0;
+        position_ligne++;
+    }
+    if (position_ligne >= LIG_MAX) {
+
+        // Gestion du défilement
+        memmove(ptr_mem(0, 0), ptr_mem(1, 0), COL_MAX * (LIG_MAX - 1) * 2);
+        for (uint32_t x = 0; x < COL_MAX; x++) {
+            ecrit_car(LIG_MAX - 1, x, ' ', 15, 0, 5);
+        }
+        position_ligne = LIG_MAX - 1;
+    }
+
+    place_curseur(position_ligne, position_colonne);
+
+
 }
 
 /*
--- ecran.h
+++ ecran.h
@@ -12,7 +12,6 @@
 
 uint16_t *ptr_mem(uint32_t lig, uint32_t col);
 
-// void ecrit_car(uint32_t lig, uint32_t col, char c);
 void ecrit_car(uint32_t lig, uint32_t col, char c,  uint32_t couleur_caractere, uint32_t couleur_fond, uint32_t clignotement);
 
 void efface_ecran(void);
